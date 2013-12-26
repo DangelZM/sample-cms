cfb0088910dee95d30a121106b175ea3dd1cbe84
Ivan Z <dangelzm@gmail.com>
2013-12-26 17:26:40 +0200

    fix config for openshift work

--- php/index.php
+++ php/index.php
@@ -9,12 +9,18 @@
         $route = $_GET['route'];
     };
 
+    if (is_file('../config.php')) {
+        $config_array = require_once('config.php');
+    } else {
+        $config_array = require_once('config.def.php');
+    }
+
     include_once("../system/start_engine.php"); // запускаем движек
     include_once("../system/config.php"); // класс - конфига
 
     //TODO add Registry Class
 
-    $config = new Config(include_once("../config.php"));
+    $config = new Config($config_array);
     $router = new Router($route, $config->get('router'));
     $layout = new Layout($config->get('template'));
     $app = new App($layout);
